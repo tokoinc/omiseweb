c7066b6293ec71573d1030690ee8694c5c614f9a
toko-ivt <kizawa@omiseweb.com>
2025-10-03 00:09:58 +0900

    Update sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -4,8 +4,8 @@ import {defineCliConfig} from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROJECT_ID!,
-    dataset: process.env.SANITY_DATASET || 'production',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+    dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
   studioHost: 'omiseweb'
 });
